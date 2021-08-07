c57c729366127a94b161d333a98759aab2f7e40d
gocalvin <pavan.kumar.k@gmail.com>
2021-08-07 16:45:05 +0530

    Added print to test

--- conf/credentials.py
+++ conf/credentials.py
@@ -6,7 +6,7 @@ def get_env_credentials():
     wc_key = os.environ.get('WC_KEY')
     wc_secret = os.environ.get('WC_SECRET')
 
-    if not wc_key or not wc_secret:
-        raise Exception("API Credentials are not added in ENV")
-    else:
-        return {'wc_key': wc_key, 'wc_secret': wc_secret}
+    #if not wc_key or not wc_secret:
+     #   raise Exception("API Credentials are not added in ENV")
+    #else:
+    return {'wc_key': wc_key, 'wc_secret': wc_secret}
